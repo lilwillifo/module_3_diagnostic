33bacc8db400722c24639cd87a4e930dcaf190d6
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-16 10:34:16 -0600

    creating station poro

--- app/models/search.rb
+++ app/models/search.rb
@@ -3,9 +3,16 @@ class Search < ActiveRecord::Base
     response = conn.get("api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['API_KEY']}&location=80203&radius=6.0&fuel_type=ELEC,LPG")
     #this gives all stations in zip code, then I'll need to sort by distance, and pull top 10
     raw_stations = JSON.parse(response.body)['fuel_stations']
-
     sorted = raw_stations.sort_by {|station| station['distance']}
     top_10 = sorted[0..9]
+    top_10.map do |raw_station|
+      Station.new({   id: raw_station['id'],
+                      name: raw_station['name'],
+                      address: raw_station['address'],
+                      fuel_type: raw_station['fuel_type'],
+                      distance: raw_station['distance'],
+                      access_days_time: raw_station['access_days_time']})
+                end
   end
 
 
@@ -13,4 +20,16 @@ class Search < ActiveRecord::Base
     Faraday.new("https://developer.nrel.gov/")
   end
 
+end
+
+class Station
+  attr_reader :id, :name, :address, :fuel_type, :distance, :access_days_time
+  def initialize(data)
+    @id = data['id']
+    @name = data['name']
+    @address = data['address']
+    @fuel_type = data['fuel_type']
+    @distance = data['distance']
+    @access_days_time = data['access_days_time']
+  end
 end
