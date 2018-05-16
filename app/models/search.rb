337b3eade802a79dbeecb21fec7ffe1bc961877a
Margaret Williford <margaretwilliford4@gmail.com>
2018-05-16 10:10:17 -0600

    pseudocoding api call

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,10 @@
 class Search < ActiveRecord::Base
+  def stations
+  end
+  # location=80203&radius=6.0&fuel_type=ELEC,LPG
+  #this gives all stations in zip code, then I'll need to sort by distance, and pull top 10
+
+  def conn
+    Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['API_KEY']}", headers: headers)
+  end
 end
